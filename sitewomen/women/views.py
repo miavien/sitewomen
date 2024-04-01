edc5a89d8a70c42c5e01f2a511d1ba628e13d68f
miavien <ki.llisaan@gmail.com>
2024-04-01 18:39:14 +0700

    new fichi

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,18 +1,47 @@
-from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponsePermanentRedirect
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.urls import reverse
+
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статью', 'url_name': 'add_page'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'}
+]
+
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Страница приложения women')
+    #альтернативный способ
+    # t = render_to_string('women/index.html')
+    # return HttpResponse(t)
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'posts': data_db,
+    }
+    return render(request, 'women/index.html', context=data)
 
-def categories(request, cat_id):
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
+def about(request):
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
-def categories_by_slug(request, cat_slug):
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
 
-def archive(request, year):
-    return HttpResponse(f'<h1>Архив по годам</h1><p>year: {year}</p>')
+def addpage(request):
+    return HttpResponse('Добавление статьи')
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+def login(request):
+    return HttpResponse('Авторизация')
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
