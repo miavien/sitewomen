b439c79198b117e7b72d87c42a958d8053404e32
miavien <ki.llisaan@gmail.com>
2024-08-13 09:39:16 +0700

    admin all done

--- site_women/women_app/admin.py
+++ site_women/women_app/admin.py
@@ -1,19 +1,62 @@
 from django.contrib import admin
+from django.db.models.functions import Length
 from .models import *
+
+
 # Register your models here.
+
+class MarriedFilter(admin.SimpleListFilter):
+    title = 'Статус женщин'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('married', 'Замужем'),
+            ('single', 'Не замужем')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        if self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
+
 
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'is_published', 'cat')
-    #для кликабельности
-    list_display_links = ('id', 'title')
-    #чтобы менять значение в списке всех строк
+    list_display = ('title', 'time_create', 'is_published', 'cat', 'bring_info')
+    # для кликабельности
+    list_display_links = ('title',)
+    # чтобы менять значение в списке всех строк
     list_editable = ('is_published', 'cat')
+    actions = ('set_published', 'set_draft')
+    search_fields = ('title', 'cat__name')
+    list_filter = (MarriedFilter, 'cat__name', 'is_published')
+    exclude = ('is_published',)
+    readonly_fields = ('slug',)
+    filter_horizontal = ('tags',)
+
+    # для пользовательского поля
+    # сортировка в данном случае по кол-ву символов поля
+    @admin.display(description='Краткое описание', ordering=Length('content'))
+    def bring_info(self, women: Women):
+        return f'Описание {len(women.content)} символов'
+
+    @admin.action(description='Опубликовать выбранные записи')
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.PUBLISHED)
+        self.message_user(request, f'Изменено {count} записей')
+
+    @admin.action(description='Отправить в черновик выбранные записи')
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(request, f'Изменено {count} записей')
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     list_display_links = ('id', 'name')
     ordering = ('id',)
-
 
 admin.site.site_header = 'Панель администрирования'
--- site_women/women_app/models.py
+++ site_women/women_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
+from unidecode import unidecode
+
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -36,6 +39,11 @@ class Women(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        transliterated_title = unidecode(self.title)
+        self.slug = slugify(transliterated_title)
+        super().save(*args, **kwargs)
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='Название')
